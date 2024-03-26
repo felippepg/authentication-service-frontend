f806848e27c4f2cbb5e3fd0b67b1ee95c5e33f1e
Felippe <felippepg.goncalves@outlook.com.br>
2024-03-25 22:48:25 -0300

    Atualizando gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
-  return (
-    <>
-      <p>teste</p>
-    </>
-  );
+  return <BrowserRouter></BrowserRouter>;
 }
 
 export default App;
